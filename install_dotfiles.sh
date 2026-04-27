0b769ffbf219fdc09b67b50e4a985bf323f745df
Clément Courcelle <clement.courcelle@inrae.fr>
2026-04-27 09:37:49 +0200

    fix install script

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-this_dir=SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-for d in ./*/; do
-	stow -d ~/dotfiles -t ~ "$(basename "$d")"
+this_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+for d in "$this_dir"/*/; do
+	stow -d "$this_dir" -t $HOME "$(basename "$d")"
 done
+
+# .gitignore is in stow ignore list
+ln -fs "$(realpath --relative-to="$HOME" "$this_dir/git/.gitignore")"
